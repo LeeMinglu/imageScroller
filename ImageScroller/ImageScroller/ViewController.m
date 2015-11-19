d9c3226d6dfcb82b1e75e95499145e1170205518
LeeMinglu <402007353@qq.com>
2015-11-19 22:38:36 +0800

    添加uiimageView,并可进行滚动

--- ImageScroller/ImageScroller/ViewController.m
+++ ImageScroller/ImageScroller/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
 
 @end
 
@@ -16,7 +17,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGFloat x = 0;
+    
+    NSInteger imageCount = 5;
+    
+    CGFloat y = self.imageScrollView.bounds.origin.y;
+    CGFloat width = self.imageScrollView.bounds.size.width;
+    CGFloat height = self.imageScrollView.bounds.size.height;
+    
+    //设置scrollView的contentSize
+    self.imageScrollView.contentSize = CGSizeMake(5 * width, 0);
+    self.imageScrollView.bounces = NO;
+    self.imageScrollView.pagingEnabled = YES;
+    self.imageScrollView.showsHorizontalScrollIndicator = NO;
+    
+    //添加UIImageView
+    for (int i = 0; i < imageCount; i++) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        x += i * self.imageScrollView.bounds.size.width;
+        
+        CGRect frame = CGRectMake(x, y, width, height);
+        
+        imageView.frame = frame;
+        
+        imageView.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+        
+        [self.imageScrollView addSubview:imageView];
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
