400d5978a1d2980bc5b5c05e3bcdb6db42920a9b
LeeMinglu <402007353@qq.com>
2015-11-19 22:22:32 +0800

    初始化项目

---
+++ ImageScroller/ImageScroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageScroller
+//
+//  Created by 李明禄 on 15/11/19.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageScroller/ImageScroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageScroller
+//
+//  Created by 李明禄 on 15/11/19.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
